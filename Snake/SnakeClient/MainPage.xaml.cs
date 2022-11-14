9901d60b508ce2ed0f568be67ebac8de363b926d
shem-snow <shem-snow@hotmail.com>
2022-11-14 15:39:07 -0700

    Created a snake solution and copied the skeleton code over.

---
+++ Snake/SnakeClient/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿namespace SnakeGame;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        graphicsView.Invalidate();
+    }
+
+    void OnTapped(object sender, EventArgs args)
+    {
+        keyboardHack.Focus();
+    }
+
+    void OnTextChanged(object sender, TextChangedEventArgs args)
+    {
+        Entry entry = (Entry)sender;
+        String text = entry.Text.ToLower();
+        if (text == "w")
+        {
+            // Move up
+        }
+        else if (text == "a")
+        {
+            // Move left
+        }
+        else if (text == "s")
+        {
+            // Move down
+        }
+        else if (text == "d")
+        {
+            // Move right
+        }
+        entry.Text = "";
+    }
+
+    private void NetworkErrorHandler()
+    {
+        DisplayAlert("Error", "Disconnected from server", "OK");
+    }
+
+
+    /// <summary>
+    /// Event handler for the connect button
+    /// We will put the connection attempt logic here in the view, instead of the controller,
+    /// because it is closely tied with disabling/enabling buttons, and showing dialogs.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    private void ConnectClick(object sender, EventArgs args)
+    {
+        if (serverText.Text == "")
+        {
+            DisplayAlert("Error", "Please enter a server address", "OK");
+            return;
+        }
+        if (nameText.Text == "")
+        {
+            DisplayAlert("Error", "Please enter a name", "OK");
+            return;
+        }
+        if (nameText.Text.Length > 16)
+        {
+            DisplayAlert("Error", "Name must be less than 16 characters", "OK");
+            return;
+        }
+        DisplayAlert("Delete this", "Code to connect to server goes here", "OK");
+
+        keyboardHack.Focus();
+    }
+
+
+    private void ControlsButton_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Controls",
+                     "W:\t\t Move up\n" +
+                     "A:\t\t Move left\n" +
+                     "S:\t\t Move down\n" +
+                     "D:\t\t Move right\n",
+                     "OK");
+    }
+
+    private void AboutButton_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("About",
+      "SnakeGame solution\nArtwork by Jolie Uk and Alex Smith\nGame design by Daniel Kopta and Travis Martin\n" +
+      "Implementation by ...\n" +
+        "CS 3500 Fall 2022, University of Utah", "OK");
+    }
+
+    private void ContentPage_Focused(object sender, FocusEventArgs e)
+    {
+        if (!connectButton.IsEnabled)
+            keyboardHack.Focus();
+    }
+}
---
+++ Snake/SnakeClient/WorldPanel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using IImage = Microsoft.Maui.Graphics.IImage;
+#if MACCATALYST
+using Microsoft.Maui.Graphics.Platform;
+#else
+using Microsoft.Maui.Graphics.Win2D;
+#endif
+using Color = Microsoft.Maui.Graphics.Color;
+using System.Reflection;
+using Microsoft.Maui;
+using System.Net;
+using Font = Microsoft.Maui.Graphics.Font;
+using SizeF = Microsoft.Maui.Graphics.SizeF;
+
+namespace SnakeGame;
+public class WorldPanel : IDrawable
+{
+    private IImage wall;
+    private IImage background;
+
+    private bool initializedForDrawing = false;
+
+#if MACCATALYST
+    private IImage loadImage(string name)
+    {
+        Assembly assembly = GetType().GetTypeInfo().Assembly;
+        string path = "SnakeGame.Resources.Images";
+        return PlatformImage.FromStream(assembly.GetManifestResourceStream($"{path}.{name}"));
+    }
+#else
+  private IImage loadImage( string name )
+    {
+        Assembly assembly = GetType().GetTypeInfo().Assembly;
+        string path = "SnakeGame.Resources.Images";
+        var service = new W2DImageLoadingService();
+        return service.FromStream( assembly.GetManifestResourceStream( $"{path}.{name}" ) );
+    }
+#endif
+
+    public WorldPanel()
+    {
+    }
+
+    private void InitializeDrawing()
+    {
+        wall = loadImage( "WallSprite.png" );
+        background = loadImage( "Background.png" );
+        initializedForDrawing = true;
+    }
+
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        if ( !initializedForDrawing )
+            InitializeDrawing();
+        
+        canvas.DrawImage(wall, 0, 0, wall.Width, wall.Height);
+    }
+}
