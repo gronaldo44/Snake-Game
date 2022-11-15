f9a36692148bb91bbc5ccc2ee7f30e37e7ef7707
shem-snow <shem-snow@hotmail.com>
2022-11-15 15:39:36 -0700

    Put TODOs over the MainPage.xaml.cs

--- Snake/SnakeClient/MainPage.xaml.cs
+++ Snake/SnakeClient/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿namespace SnakeGame;
+using NetworkUtil;
+using Windows.Gaming.Input;
 
 public partial class MainPage : ContentPage
 {
@@ -8,37 +10,73 @@ public partial class MainPage : ContentPage
         graphicsView.Invalidate();
     }
 
+    /// <summary>
+    /// This just makes so when you click on text entries, the program focuses on them so you can type in them.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
     void OnTapped(object sender, EventArgs args)
     {
         keyboardHack.Focus();
     }
 
+    /// <summary>
+    /// This just makes so when you click on text entries, the program focuses on them andyou can start typing in them.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void ContentPage_Focused(object sender, FocusEventArgs e)
+    {
+        if (!connectButton.IsEnabled)
+            keyboardHack.Focus();
+    }
+
+    /// <summary>
+    /// This method is called every time the user types something into the text box that controls the snake.
+    /// It reads the first letter then clears the entry's text.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
     void OnTextChanged(object sender, TextChangedEventArgs args)
     {
         Entry entry = (Entry)sender;
         String text = entry.Text.ToLower();
         if (text == "w")
         {
-            // Move up
+            // TODO: Move up
         }
         else if (text == "a")
         {
-            // Move left
+            // TODO: Move left
         }
         else if (text == "s")
         {
-            // Move down
+            // TODO: Move down
         }
         else if (text == "d")
         {
-            // Move right
+            // TODO: Move right
         }
         entry.Text = "";
     }
 
+    /// <summary>
+    /// The callback for when NetworkErrors occur. This method disconnects from the server and re-enables the 
+    /// controls for the user to connect.
+    /// </summary>
     private void NetworkErrorHandler()
     {
-        DisplayAlert("Error", "Disconnected from server", "OK");
+        // Show the error
+        Dispatcher.Dispatch(() => DisplayAlert("Error", "Disconnected from server", "OK"));
+
+        // Then re-enable the controlls so the user can reconnect to a new server with a new name.
+        Dispatcher.Dispatch(
+          () =>
+          {
+              connectButton.IsEnabled = true;
+              serverText.IsEnabled = true;
+              nameText.IsEnabled = true;
+          });
     }
 
 
@@ -66,12 +104,29 @@ public partial class MainPage : ContentPage
             DisplayAlert("Error", "Name must be less than 16 characters", "OK");
             return;
         }
-        DisplayAlert("Delete this", "Code to connect to server goes here", "OK");
+
+        // TODO: Attempt to connect to the server.
+        // GameController.connect();
+
+        // TODO: { only do the following if connection to the server was successful.
+
+        // Disable the connect button and text entries so the user can't connect again or change the server or their name.
+        connectButton.IsEnabled = false;
+        serverText.IsEnabled = false;
+        nameText.IsEnabled = false;
+
+        // TODO: }
+
+        
 
         keyboardHack.Focus();
     }
 
-
+    /// <summary>
+    /// Displays the controlls when a player clicks on "help".
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void ControlsButton_Clicked(object sender, EventArgs e)
     {
         DisplayAlert("Controls",
@@ -82,17 +137,18 @@ public partial class MainPage : ContentPage
                      "OK");
     }
 
+    /// <summary>
+    /// Displays the creation information about this MAUI program when the "about" button is clicked.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void AboutButton_Clicked(object sender, EventArgs e)
     {
         DisplayAlert("About",
       "SnakeGame solution\nArtwork by Jolie Uk and Alex Smith\nGame design by Daniel Kopta and Travis Martin\n" +
-      "Implementation by ...\n" +
+      "Implementation by Ronald Foster and Shem Snow\n" +
         "CS 3500 Fall 2022, University of Utah", "OK");
     }
 
-    private void ContentPage_Focused(object sender, FocusEventArgs e)
-    {
-        if (!connectButton.IsEnabled)
-            keyboardHack.Focus();
-    }
+    
 }
