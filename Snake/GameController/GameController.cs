03a97f585819520ead080efcdc29b723066dac71
gronaldo44 <gronaldo44@gmail.com>
2022-11-22 16:49:49 -0700

    Improved speed of OnFrame method by not sending a request to the server unless there is a change

--- Snake/GameController/GameController.cs
+++ Snake/GameController/GameController.cs
@@ -10,12 +10,17 @@ using Newtonsoft.Json.Linq;
 /// </summary>
 public class GameController
 {
-    private Action DrawWorld;                       // Draws the world based on the model
-    private SocketState connection;                 // This client's connection to the server  
+    #region Server Params
+    private Action UpdateArrived;       // Notifies the view that an update came from the server
+    private SocketState connection;     // This client's connection to the server  
+    #endregion
+    #region Client Params
     private string playerName;
     private World theWorld;
-    #region JSON Properties
-    private string moving;           // What direction the player is moving in
+    #endregion
+    #region Control Commands
+    private bool clientPressedCommand = false;
+    private string moving;              // What direction the player is moving in
     #endregion
 
     /// <summary>
@@ -24,7 +29,7 @@ public class GameController
     /// <param name="draw"></param>
     public GameController(Action draw, World w)
     {
-        DrawWorld = draw;
+        UpdateArrived = draw;
         connection = new SocketState((s) => { }, "no connection made");
         moving = "none";
         playerName = "";    // temporary value
@@ -43,12 +48,13 @@ public class GameController
     }
 
     /// <summary>
-    /// Updates the direction the player is currently moving in
+    /// Queues a request to the server to update the player's control command
     /// </summary>
     /// <param name="dir"></param>
     public void MoveCommand(string dir)
     {
         moving = dir;
+        clientPressedCommand = true;
     }
 
     /// <summary>
@@ -97,8 +103,8 @@ public class GameController
                 }
             }
         }
-        // Draw the walls
-        DrawWorld();
+        // Notify the View that the walls have arrived from the server
+        UpdateArrived.Invoke();
 
         // Update on each frame
         state.OnNetworkAction = OnFrame;
@@ -112,13 +118,17 @@ public class GameController
     private void OnFrame(SocketState state)
     {
         // Only one command may be received each frame
-        string controlCommand = "{\"moving\":\"" + moving + "\"}\n";
-        Networking.Send(state.TheSocket, controlCommand);
+        if (clientPressedCommand)
+        {
+            string controlCommand = "{\"moving\":\"" + moving + "\"}\n";
+            Networking.Send(state.TheSocket, controlCommand);
+            clientPressedCommand = false;
+        }
 
         // Update the values in the world
         theWorld.UpdateWorld(state);
-        // Draw the world
-        DrawWorld();
+        // Notify the View that an update has arrived from the server
+        UpdateArrived.Invoke();
     }
 
 }
