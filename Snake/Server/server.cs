93546cebc237bb365034d057d14e90b2b7e112a5
shem-snow <shem-snow@hotmail.com>
2022-11-30 13:24:49 -0700

    Created a new project for the Server and gave it a reference to the Model.
    Also put in a few TODOs

---
+++ Snake/Server/server.cs
@@ -0,0 +1,48 @@
+﻿namespace SnakeGame;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+/// <summary>
+/// TODO: Header comments.
+/// </summary>
+internal class server
+{
+
+    // TODO: Need a collection of moveable objects. Or just access to the world.
+    World theWorld;
+
+    /**
+     * TODO: Need a collection of all commands that need to be executed on this frame.
+     * Or maybe we could just change an attribute (such as direction or boolean flag) when the 
+     * command is received then update the objects' appearance at the start of each frame.
+     */
+
+    /// <summary>
+    /// TODO: Default Constructor
+    /// </summary>
+    public server()
+    {
+        theWorld = new();
+    }
+
+    /// <summary>
+    /// TODO: contract
+    /// </summary>
+    private void OnFrame()
+    {
+        // TODO: update the state (movement, position, booleans) of each object
+        foreach (object obj in theWorld)
+        {
+            // if(dead) remove
+            // else obj.move()  or obj.update()
+        }
+    }
+
+}
+
