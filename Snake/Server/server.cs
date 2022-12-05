62a56c5922c9e779009591b8923ef2b9191ca83c
gronaldo44 <gronaldo44@gmail.com>
2022-12-05 16:28:46 -0700

    Started respawning snakes after they die. Snakes are only set to died for one frame.

--- Snake/Server/server.cs
+++ Snake/Server/server.cs
@@ -14,6 +14,7 @@ using System.Text.RegularExpressions;
 using System.Security.Cryptography;
 using Newtonsoft.Json.Linq;
 using System.Xml.Linq;
+using System.Data;
 
 
 
@@ -22,7 +23,6 @@ using System.Xml.Linq;
 /// </summary>
 public class Server
 {
-    private static World theWorld = new();
     private static ServerController controller = new();
 
     /*
@@ -43,56 +43,6 @@ public class Server
     /// </summary>
     public Server()
     {
-        // Get the current settings
-        XmlDocument settings = new();
-        settings.Load("settings.xml");
-        // Read the frame settings and world size
-        XmlNode? fpshot = settings.SelectSingleNode("//GameSettings/FramesPerShot");
-        if (fpshot != null)
-        {
-            theWorld.FramesPerShot = int.Parse(fpshot.InnerText);
-        }
-        XmlNode? mspframe = settings.SelectSingleNode("//GameSettings/MSPerFrame");
-        if (mspframe != null)
-        {
-            theWorld.MSPerFrame = int.Parse(mspframe.InnerText);
-        }
-        XmlNode? respawnRate = settings.SelectSingleNode("//GameSettings/RespawnRate");
-        if (respawnRate != null)
-        {
-            theWorld.RespawnRate = int.Parse(respawnRate.InnerText);
-        }
-        XmlNode? univereSize = settings.SelectSingleNode("//GameSettings/UniverseSize");
-        if (univereSize != null)
-        {
-            theWorld.worldSize = int.Parse(univereSize.InnerText);
-        }
-        // Read the walls node
-        XmlNode? wallsNode = settings.SelectSingleNode("//GameSettings/Walls");
-        if (wallsNode != null)
-        {   // Read each wall
-            XmlNodeList walls = wallsNode.ChildNodes;
-            foreach (XmlNode w in walls)
-            {
-                XmlNode? id = w.SelectSingleNode("ID");
-                XmlNode? p1_x = w.SelectSingleNode("p1/x");
-                XmlNode? p1_y = w.SelectSingleNode("p1/y");
-                XmlNode? p2_x = w.SelectSingleNode("p2/x");
-                XmlNode? p2_y = w.SelectSingleNode("p2/y");
-                if (id != null && p1_x != null && p1_y != null && p2_x != null &&
-                    p2_y != null)
-                {
-                    Wall wall = new();
-                    wall.id = int.Parse(id.InnerText);
-                    wall.p1.X = double.Parse(p1_x.InnerText);
-                    wall.p1.Y = double.Parse(p1_y.InnerText);
-                    wall.p2.X = double.Parse(p2_x.InnerText);
-                    wall.p2.Y = double.Parse(p2_y.InnerText);
-                    theWorld.walls.Add(wall.id, wall);
-                }
-            }
-            Console.WriteLine("break");
-        }
     }
 
     /// <summary>
@@ -103,37 +53,31 @@ public class Server
     {
         // Get the game settings
         Server s = new();
-
         // Start listening for and receiving snake-clients
-        TcpListener listener = Networking.StartServer(controller.ProcessClientConnection, 11000);
+        TcpListener listener = Networking.StartServer(controller.ClientConnection, 11000);
 
-        // TODO: Finish connection with the client (send them their id, worldsize, and walls).... I think this is done in "OnConnect".
+        // Start updating and broadcasting the state of the world to each client
+        Stopwatch serverFPS = new(), frameTimer = new();
+        frameTimer.Start();
+        serverFPS.Start();
+        while (true)
+        {   // Loop for each frame until the server is stopped
+            controller.UpdateWorld();
+            controller.BroadcastWorld();
+            
+            // Print the current framerate the server is running at
+            if (serverFPS.ElapsedMilliseconds >= 1000)
+            {
+                Console.WriteLine("FPS: " + (1000 / controller.theWorld.MSPerFrame));
+                serverFPS.Restart();
+            }
 
-
-        // TODO: Start sending onFrame to the clients
-        /*
-         * while(there are no clients connected) {spin/do nothing}
-         * 
-         * while(there are clients connected) {OnFrame/Update()}
-         */
-
-        /*
-         * TODO: Allow the client to send move commands ........ 
-         * 
-         * The assignment instructions say
-         *      "The commands should be applied on the next frame after receiving them"
-         *      
-         * So I think we should handle this in OnFrame/Update()
-         * 
-         *      - maybe we could save each move command in a variable (Snakes already have a "dir") 
-         *      then at the start of the for-each-client-loop in OnFrame:
-         *          
-         *          World.snakes[ID].position += velocity * dir;
-         *      
-         *          if(the new command is valid)
-         *              World.snakes[ID].dir = new command;
-         */
-
+            while (frameTimer.ElapsedMilliseconds < controller.theWorld.MSPerFrame)
+            {
+                // Wait for the frame to finish
+            }
+            frameTimer.Restart();
+        } 
     }
 
     #endregion
